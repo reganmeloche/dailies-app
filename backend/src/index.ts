73cb7d17c9aeb398aeb77cf7f48b96d7c03c8009
reganmeloche <reganmeloche@gmail.com>
2024-12-14 16:04:09 -0500

    prep for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,22 +3,14 @@ import { CategoryEnum } from './helpers/initialCategories';
 import setup from './setup';
 import { Config } from './config';
 import path from 'path';
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware to parse JSON
 app.use(express.json());
-
-
-// Front end setup
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "build")));
-
-// Handle React routing, return index.html for unknown routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 // Get config
 let config: Config;
@@ -42,6 +34,17 @@ app.get('/api/:category', async (req, res) => {
     const result = await cacheLib.get(category, req.query.clear == 'true');
     res.json(result);
 });
+
+// Front end setup
+if (process.env.NODE_ENV !== 'development') {
+  // Serve static files from the React app
+  app.use(express.static(path.join(__dirname, "build")));
+
+  // Handle React routing, return index.html for unknown routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
